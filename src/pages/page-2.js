9f53d8d8b55e8559c4af342ba2f4c85230cc7d64
FarazAfridi <farazafridi5159@gmail.com>
2020-11-15 14:19:15 +0500

    netlify-deploy

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,10 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = () => 
+  (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the </h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
